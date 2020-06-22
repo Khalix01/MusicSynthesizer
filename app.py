40400a40f0a25a1794de81522fbce86b203f168f
krutik shah <krutikshah01@gmail.com>
2020-06-22 01:05:03 -0400

    random generation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import wave, struct, math
+import wave, struct, math, random
 from flask import Flask, escape, request, render_template, jsonify, send_file
 import threading
 import time
@@ -132,15 +132,62 @@ def readMusic(file='h.wav'):
 			form.append(d[j])
 	wavef.close()
 
-file=files[1]
+def writeNote(tempo, len, rate, freq, f, stime=0.1):
+    value = 0
+    for i in range(int(60 / tempo * (len) * rate)):
+        value = int(32767.0 * math.cos(freq * math.pi * float(i) / float(rate)))
+        data = struct.pack('<h', value)
+        f.writeframesraw(data)
+    dv = (32767.0 - value) / int(stime * rate)
+    for i in range(int(stime * rate)):
+        f.writeframesraw(struct.pack('<h', int(value + (i * dv))))
+
+
+def writeNotes(tempo, l, rate, freqs, f, stime=0.1):
+    value = 0
+    for i in range(int((int(60 / tempo * (l) * rate)))):
+        value = 0
+        for k in freqs:
+            value += (math.cos(Notes[k] * math.pi * float(i) / float(rate)))
+        value = int(32767.0 * (value / len(freqs)))
+        # print(value)
+        data = struct.pack('<h', value)
+        f.writeframesraw(data)
+    dv = (32767.0 - value) / int(stime * rate)
+    for i in range(int(stime * rate)):
+        f.writeframesraw(struct.pack('<h', int(value + (i * dv))))
+
+def randMusic(str):
+    tempo = 120
+    sampleRate = 44100.0
+    c = {2: noteToFreq["C5"], 3: noteToFreq["D5"], 5: noteToFreq["E5"], 1: noteToFreq["F5"], 6: noteToFreq["G5"], 7: noteToFreq["A5"], 4: noteToFreq["B5"]}
+    wavef = wave.open('static/'+ str, 'w')
+    wavef.setnchannels(1)  # mono
+    wavef.setsampwidth(2)
+    wavef.setframerate(sampleRate)
+    # line = reader.readline()
+    # while line != '':
+    #     writeNotes(tempo, int(line.split()[1]), sampleRate, line.split()[0].split(","), wavef)
+    #     line = reader.readline()
+    for i in range(random.randint(5, 30)):
+        writeChord(tempo, random.randint(1, 4), sampleRate, c[random.randint(1, 7)], wavef)
+    dv = (32767.0) / int(0.1 * sampleRate)
+    for i in range(int(0.1 * sampleRate)):
+        wavef.writeframesraw(struct.pack('<h', int(32767 - (i * dv))))
+    writeNote(tempo, 1, sampleRate, c[1], wavef)
+    wavef.writeframes(struct.pack('<h', 0))
+    wavef.close()
+
+file=files[-1]
 @app.route('/')
 @app.route('/home')
-def home():
+def home(f=file):
 	global i
 	global form
-	readMusic(file)
+	readFreqs()
+	readMusic(f)
 	#print(file)
-	return render_template('wave.html', length = length,form=form[i:], file = file)
+	return render_template('wave.html', length = min(length,len(form[i:])),form=form[i:], file = f)
 
 @app.route('/playWave', methods=['POST'])
 def update():
@@ -159,18 +206,20 @@ def reset():
 	i=0
 	return render_template('layout_1.html', length = length,form=form[i:])
 
-
 @app.route('/updateFile', methods=['POST'])
 def fileUpdate():
-	files.append(request.get_json()['file'])
+	if request.get_json()['file'] not in files:
+		files.append(request.get_json()['file'])
 	writer=open("static/files.txt", 'w')
 	for filename in files:
-		writer.write(filename+"\n")
+		if (filename != ''):
+			writer.write(filename+"\n")		
 	writer.close()
-	home()
+	randMusic(files[-1])
+	file=files[-1]
 	global i
 	i=0
-	return render_template('layout_1.html', length = length,form=form[i:])
+	return home(request.get_json()['file'])
 
 @app.route('/download')
 def downloadFile ():
